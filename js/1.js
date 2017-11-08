d315734a8b59ba33d96dd86777eea79e3c6c9988
YukhymenkoArtur <yukhymenko.artur@gmail.com>
2017-11-08 13:04:05 +0200

    start

---
+++ js/1.js
@@ -0,0 +1,10 @@
+function tamagotchi (name) {
+	this.name = name;
+	this.health = 100;
+	this.food = 100;
+	this.power = 100;
+	this.happy = 100;
+	this.toilet = 100;
+}
+
+let player = new tamagotchi ("Picachu");
