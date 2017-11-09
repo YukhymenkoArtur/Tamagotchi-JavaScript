97ad5373a05c0fc6b48202db7b7cd3fca4aeef72
YukhymenkoArtur <yukhymenko.artur@gmail.com>
2017-11-09 14:20:33 +0200

    09.11

--- js/1.js
+++ js/1.js
@@ -1,10 +1,54 @@
-function tamagotchi (name) {
-	this.name = name;
-	this.health = 100;
-	this.food = 100;
-	this.power = 100;
-	this.happy = 100;
-	this.toilet = 100;
+function tamagotchi() {
+  this.name = name;
+  this.hearth = 100;
+  this.food = 100;
+  this.power = 100;
+  this.happy = 100;
+  this.toilet = 100;
+
 }
 
-let player = new tamagotchi ("Picachu");
+function timer () {
+
+}
+
+var tamagotchi = new tamagotchi("Picachu");
+
+// ф-ції для підвищення показників
+
+function hearth () {
+	if (this.hearth < 25) {
+		this.hearth++;
+	};
+}
+
+function feed () {
+	if (this.food < 25) {
+		this.food++;
+	};
+}
+
+function power () {
+	if (this.power < 25) {
+		this.power++;
+	};
+}
+
+function happy () {
+	if (this.happy < 25) {
+		this.happy++;
+	};
+}
+
+function toilet () {
+	if (this.toilet < 25) {
+		this.toilet++;
+	};
+}
+
+
+// ф-ції "предсмертного" стану тамагочі
+
+
+
+document.getElementById('....')
